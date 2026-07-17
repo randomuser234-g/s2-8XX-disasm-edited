afdd20e15c3bad194207f13798d05a15b038d32a
randomuser234-g <thoratkihome@gmail.com>
2026-07-17 17:58:08 -0500

    mappings for title menu
    unlike sonic 2, this pulls text from already existing error font rather than dedicated "1 PLAYER" graphic to be loaded into vram

---
+++ mappings/sprite/obj8B.asm
@@ -0,0 +1,57 @@
+Map_d994: mappingsTable
+	mappingsTableEntry.w	Map_d994_0
+	mappingsTableEntry.w	Map_d994_1
+	mappingsTableEntry.w	Map_d994_2
+
+Map_d994_0:	spriteHeader
+ spritePiece -$2C, -$C, 1, 1, 1, 0, 0, 2, 0
+ spritePiece -$3D, -$C, 1, 1, $D, 0, 0, 2, 0
+ spritePiece -$1C, -$C, 1, 1, $20, 0, 0, 2, 0
+ spritePiece -$14, -$C, 1, 1, $1C, 0, 0, 2, 0
+ spritePiece -$C, -$C, 1, 1, $11, 0, 0, 2, 0
+ spritePiece -4, -$C, 1, 1, $F, 0, 0, 2, 0
+ spritePiece 4, -$C, 1, 1, $15, 0, 0, 2, 0
+ spritePiece $C, -$C, 1, 1, $22, 0, 0, 2, 0
+ spritePiece -$2C, 4, 1, 1, $1F, 0, 0, 2, 0
+ spritePiece -$24, 4, 1, 1, $20, 0, 0, 2, 0
+ spritePiece -$1C, 4, 1, 1, $24, 0, 0, 2, 0
+ spritePiece -$14, 4, 1, 1, $19, 0, 0, 2, 0
+ spritePiece -$C, 4, 1, 1, $1F, 0, 0, 2, 0
+ spritePiece -4, 4, 1, 1, $1E, 0, 0, 2, 0
+ spritePiece 4, 4, 1, 1, $23, 0, 0, 2, 0
+Map_d994_0_End
+
+Map_d994_1:	spriteHeader
+ spritePiece -$2C, -$C, 1, 1, 1, 0, 0, 2, 0
+ spritePiece -$3D, 4, 1, 1, $D, 0, 0, 2, 0
+ spritePiece -$1C, -$C, 1, 1, $20, 0, 0, 2, 0
+ spritePiece -$14, -$C, 1, 1, $1C, 0, 0, 2, 0
+ spritePiece -$C, -$C, 1, 1, $11, 0, 0, 2, 0
+ spritePiece -4, -$C, 1, 1, $F, 0, 0, 2, 0
+ spritePiece 4, -$C, 1, 1, $15, 0, 0, 2, 0
+ spritePiece $C, -$C, 1, 1, $22, 0, 0, 2, 0
+ spritePiece -$2C, 4, 1, 1, $1F, 0, 0, 2, 0
+ spritePiece -$24, 4, 1, 1, $20, 0, 0, 2, 0
+ spritePiece -$1C, 4, 1, 1, $24, 0, 0, 2, 0
+ spritePiece -$14, 4, 1, 1, $19, 0, 0, 2, 0
+ spritePiece -$C, 4, 1, 1, $1F, 0, 0, 2, 0
+ spritePiece -4, 4, 1, 1, $1E, 0, 0, 2, 0
+ spritePiece 4, 4, 1, 1, $23, 0, 0, 2, 0
+Map_d994_1_End
+
+Map_d994_2:	spriteHeader
+ spritePiece -$3D, 0, 1, 1, $D, 0, 0, 2, 0
+ spritePiece 3, 0, 1, 1, $23, 0, 0, 2, 0
+ spritePiece -$2D, 0, 1, 1, $1C, 0, 0, 2, 0
+ spritePiece -$25, 0, 1, 1, $15, 0, 0, 2, 0
+ spritePiece -$1D, 0, 1, 1, $26, 0, 0, 2, 0
+ spritePiece -$15, 0, 1, 1, $15, 0, 0, 2, 0
+ spritePiece -$D, 0, 1, 1, $1C, 0, 0, 2, 0
+ spritePiece $B, 0, 1, 1, $15, 0, 0, 2, 0
+ spritePiece $13, 0, 1, 1, $1C, 0, 0, 2, 0
+ spritePiece $1B, 0, 1, 1, $15, 0, 0, 2, 0
+ spritePiece $23, 0, 1, 1, $13, 0, 0, 2, 0
+ spritePiece $2B, 0, 1, 1, $24, 0, 0, 2, 0
+Map_d994_2_End
+
+	even
